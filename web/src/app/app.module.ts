71a2463c62040cb6af12c52bdbf5a5e788f5d157
FrancoGroenewald <franco@softwareworx.co.za>
2021-07-28 14:52:59 +0200

    add login component

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentComponent } from './component/component.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentComponent,
-    SidebarComponent
+    SidebarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
